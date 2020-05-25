cc9ac44566d793ab85b80b59d2bb614732298fff
NorovichukIrinaA <65623204+NorovichukIrinaA@users.noreply.github.com>
2020-05-25 22:41:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     $('#myBtn').on('click',function () {
         var weight = Number(document.getElementById("weight").value);
         var height = Number(document.getElementById("height").value);
-        if (( weight + height < 1000 ) && ( weight + height > 0 )) {
+        if ( weight + height < 1000 && weight + height > 0  ) {
             var imt = +weight / (height * height) * 10000;
             imt = imt.toFixed(2);
             document.getElementById("imt").innerHTML = imt;
