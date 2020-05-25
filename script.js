b8c438a2d1c45cf4e85d1ec355a95ddbfd8b0759
NorovichukIrinaA <65623204+NorovichukIrinaA@users.noreply.github.com>
2020-05-25 23:40:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+ $(document).ready(function() {
+    $('#myBtn').on('click',function () {
+        var weight = Number(document.getElementById("weight").value);
+        var height = Number(document.getElementById("height").value);
+        if (  weight + height < 1000  && weight + height > 1000 ) {
+            var imt = +weight / (height * height) * 10000;
+            imt = imt.toFixed(2);
+            document.getElementById("imt").innerHTML = imt;
+        } else{
+            document.getElementById("imt").innerHTML = "Неверные данные";
+        }
+    })
+
+     $(".resize img").click(function(){
+        var src = $(this).attr('src');
+        $(".popup_bg").append("<img src="+src+" class='popup_img' />");
+        $(".popup").fadeIn(500);
+        $('.popup').on('click', function(){
+            $(".popup").fadeOut(500);
+            setTimeout(function() {
+                $(".popup_img").remove();
+            }, 500);
+        });
+    });
+});
